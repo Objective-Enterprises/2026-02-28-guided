0740ad7f788096d5e801f5ab9892572f95b265d4
David Y. Stephenson <david@davidystephenson.com>
2026-02-28 12:50:02 -0500

    Define MagicalItem

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,27 @@ interface IMagicalItem {
 }
 
 // 2. Class implementing IMagicalItem
+class MagicalItem implements IMagicalItem {
+  name: string
+  type: string
+  powerLevel: number
+  isRare: boolean
 
+  constructor (name: string, type: string, powerLevel: number, isRare: boolean) {
+    this.name = name
+    this.type = type
+    this.powerLevel = powerLevel
+    this.isRare = isRare
+  }
+
+  displayInfo () {
+    const message = `${this.name} is a ${this.isRare ? 'rare' : 'common'} ${this.type} with ${this.powerLevel} power.`
+    console.log(message)
+  }
+}
+
+const bootsOfWisdom = new MagicalItem('Boots of Wisdom', 'armor', 9002, true)
+bootsOfWisdom.displayInfo()
 
 // Function to compare power levels of two items
 
